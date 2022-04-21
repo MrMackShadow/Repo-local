be084d17cc479243ff1bf6f5b8077092b63e6bbd
Tony <dinekb96@mail.ru>
2022-04-21 21:11:19 +0100

    dz_4.7 create and done

---
+++ Lesson_4/dz_4.7.py
@@ -0,0 +1,16 @@
+from math import factorial
+from itertools import count
+
+def fact():
+    for el in count(1):
+        yield factorial(el)
+
+x = 0
+n = int(input("Введите число до которого нужно посчитать: "))
+for i in fact():
+    if x < n:
+        print(i)
+        x += 1
+    else:
+        break
+fact()
