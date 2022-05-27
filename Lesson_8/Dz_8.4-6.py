4b7ce45f3466d1bd73b1d62db16298dcf9f41cef
Tony <dinekb96@mail.ru>
2022-05-27 20:51:43 +0100

    Edit Dz_8.4-6.py (Попытался сделать добавление в отдельные подразделения на складе, что то немного не получается. и вторая проблема не могу устранить то что по запросу на выход программа не выходи и продалжает цикл, в дальнейшем буду все равно переделывать)

--- Lesson_8/Dz_8.4-6.py
+++ Lesson_8/Dz_8.4-6.py
@@ -1,34 +1,21 @@
 class Sklad:
     def __init__(self):
         self.dict = {}
-        self.cop = []
-        self.pr = []
-        self.scn = []
-
-    # @classmethod
-    # def extract(cls, data):
-    #     my_date = [i for i in data.split() if i != '-']
-    #     return int(my_date[0]), int(my_date[1]), int(my_date[2])
-
-    #    @staticmethod
-    #    def valid(day, month, year):
-    #
-    #        if 1 <= day <= 31:
-    #            if 1 <= month <= 12:
-    #                if 2022 >= year >= 1900:
-    #                    return f'Дата верна!'
-    #                else:
-    #                    return f'Год введен не правильно!'
-    #            else:
-    #                return f'Месяц введен не правильно!'
-    #        else:
-    #            return f'День введен не правильно!'
-    #
-    #    def __str__(self):
-    #        return f'Введенная дата: {Data.extract(self.data)}'
+        self.cop = {}
+        self.pr = {}
+        self.scn = {}
 
     def add_to(self, equipment):
         self.dict.setdefault(equipment.name, []).append(equipment)
+
+    def add_to_cop(self, equipment):
+        self.cop.setdefault(equipment.name, []).append(equipment)
+
+    def add_to_pr(self, equipment):
+        self.pr.setdefault(equipment.name, []).append(equipment)
+
+    def add_to_scn(self, equipment):
+        self.scn.setdefault(equipment.name, []).append(equipment)
 
     def extract_1(self, name):
         if self.dict[name]:
@@ -43,6 +30,9 @@ class RoboHouse:
         self.amount = amount
         self.all_in_shop = []
         self.my_shop = []
+        self.cop = []
+        self.pr = []
+        self.scn = []
         self.my_unit = {'Модель устройства: ': self.name, 'Цена: ': self.price, 'Количество: ': self.amount}
 
     def __str__(self):
@@ -58,20 +48,32 @@ class RoboHouse:
                 total = {'Модель устройства: ': in_name, 'Цена: ': in_price, 'Количество: ': in_amount}
                 self.my_unit.update(total)
                 self.my_shop.append(self.my_unit)
-                print(f'Сейчас на складе: \n {self.my_shop}')
-
+                print(f'Весь товар на складе: \n {self.my_shop}')
+                print(f'Отдел принтеров: \n {self.pr}')
+                print(f'Отдел сканеров: \n {self.scn}')
+                print(f'Отдел ксероксов: \n {self.cop}')
             except:
                 return f'Вы ввели неккоректные данные, попробуйте еще раз'
 
-            print(f'Завершить ввод -> Q, Продолжить -> Enter')
+            print(
+                f'Завершить ввод -> Q, Перенести на склад Принтеров -> P, На склад сканеров -> S, Склад Ксероксов -> C  Продолжить -> Enter')
             q = input(f'---> ')
-            if q == 'Q' or q == "q":
+            if q != 'Q' or q != "q":
+                if q != "p" or q != "P":
+                    if q != "c" or q != "c":
+                        if q != "s" or q != "S":
+                            return RoboHouse.inter(self)
+                        else:
+                            return self.scn.append(self.my_shop)
 
+                    else:
+                        return self.cop.append(self.my_shop)
+                else:
+                    return self.pr.append(self.my_shop)
+            else:
                 self.all_in_shop.append(self.my_shop)
                 print(f'Весь склад -\n {self.all_in_shop}')
                 return f'Завершение сеанса'
-            else:
-                return RoboHouse.inter(self)
 
 
 class Printer(RoboHouse):
@@ -79,34 +81,29 @@ class Printer(RoboHouse):
         super().__init__(name, price, amount)
         self.series = series
 
-    def action(self):
-        return 'Печать документов'
-
 
 class Scaner(RoboHouse):
     def __init__(self, name, price, amount_str):
         super().__init__(name, price, amount_str)
         self.scaner_dict = []
 
-    def action(self):
-        return 'Скан документов'
-
 
 class Copier(RoboHouse):
-    def __init__(self, name, price, amount, year):
-        super().__init__(name, price, amount, year)
+    def __init__(self, name, price, amount):
+        print('Новый копировальный аппарат: ')
+        super().__init__(name, price, amount)
         self.xerox_dict = []
 
-    def action(self):
-        return 'Копирование документов'
 
-
-test_1 = Printer(322, 'hp', 2000, 5)
+printer = Printer(322, 'hp', 2000, 5)
+scaner = Scaner('Lenovo', '321', 90)
 sklad = Sklad()
-print(test_1)
-sklad.add_to(test_1)
-print(test_1.inter())
-scaner = Scaner('hp', '321', 90)
+print(printer)
+print(scaner)
+sklad.add_to(printer)
+sklad.add_to_pr(printer)
+print(sklad)
+print(printer.inter())
 sklad.add_to(scaner)
 print(sklad.dict)
 sklad.extract_1('hp')
